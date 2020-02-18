37c2576f08fdfa82ff782c7ebd4d527b9b13d46f
Ben Abbott <babbott85@gmail.com>
2020-02-18 15:07:52 -0700

    site is functional

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "cache-v1";
+
+const urlsToCache = [
+    "/",
+    "/styles.css",
+    "/index.js"
+]
+
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(urlsToCache);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  // activate
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+  }});
+  
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const compression = require("compression")
 
 const PORT = process.env.PORT || 3000;
 
